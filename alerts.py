c11783d8a25081ecda7824152a4678687dd465b6
JMal32 <justin.malone32@gmail.com>
2024-11-28 23:16:48 -0600

    started alerts script

---
+++ alerts.py
@@ -0,0 +1,7 @@
+import requests
+
+# Variables for API keys, project keys, and routing keys for PagerDuty, Slack, and Jira.
+
+pager_key = "ENTER YOUR PAGERDUTY API KEY"
+jira_key = "ENTER YOUR JIRA API KEY"
+slack_key = "ENTER YOUR SLACK 
